5bd3752cbb200ca27f15df70a79539a6ef12a737
elisabruni <72208269+elisabruni@users.noreply.github.com>
2025-07-02 08:34:34 +0000

    Deploying to gh-pages from @ elisabruni/elisabruni.github.io@1b88d475b883e5a1eb8f6e5acf3c4ae4a7471dfc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -400,9 +400,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-4-per-1000",
+          title: '4 per 1000',
+          description: "Reaching the 4‰ objective",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
